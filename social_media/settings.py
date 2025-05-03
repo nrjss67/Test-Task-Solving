adec9856dc511b32c94ed6fb5786302cb846144a
nrjss67 <extazzzyprofit@gmail.com>
2025-05-03 19:16:15 +0300

    remove db.sqlite3

--- social_media/settings.py
+++ social_media/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-pv=a)oe2*bl=97e=j2t#me^p=1wlh&!m5imehe7m3r^22*4&-0"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
